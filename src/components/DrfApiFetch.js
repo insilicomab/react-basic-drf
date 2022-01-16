679a26e7c6f9caf1d5b2d2a663c25035cc419e68
insilicomab <54536339+insilicomab@users.noreply.github.com>
2022-01-16 14:13:53 +0900

    削除機能を追加

--- src/components/DrfApiFetch.js
+++ src/components/DrfApiFetch.js
@@ -33,12 +33,28 @@ const DrfApiFetch = () => {
       });
   };
 
+  const deleteTask = (id) => {
+    axios
+      .delete(`http://127.0.0.1:8000/api/tasks/${id}/`, {
+        headers: {
+          Authorization: "Token 5996f3ff9ce3bfbe3e2d37f1ad2a2d2f1f29fe47",
+        },
+      })
+      .then((res) => {
+        setTasks(tasks.filter((task) => task.id !== id)); // 削除したタスク以外を設定
+        setSelectedTask([]); // 選択したタスクを初期化
+      });
+  };
+
   return (
     <div>
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>
             {task.title} id: {task.id}
+            <button onClick={() => deleteTask(task.id)}>
+              <i className="fas fa-trash-alt"></i>
+            </button>
           </li>
         ))}
       </ul>
@@ -54,6 +70,9 @@ const DrfApiFetch = () => {
       <button type="button" onClick={() => getTask()}>
         Get task
       </button>
+      {/* <button type="button" onClick={() => deleteTask()}>
+        Delete
+      </button> */}
       <h3>{selectedTask.title}</h3>
     </div>
   );
